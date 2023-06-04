1f477b70de1768232c1cf8b338a8cebf3e012d8b
Raquel Tsuji <raqueltsuji@gmail.com>
2023-06-04 15:17:39 -0300

    Remover Cursos

--- src/main/java/com/raquel/Semana08/controller/CourseController.java
+++ src/main/java/com/raquel/Semana08/controller/CourseController.java
@@ -47,4 +47,14 @@ public class CourseController {
                 })
                 .orElse(ResponseEntity.notFound().build());
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        return courseRepository.findById(id)
+                .map(recordFound -> {
+                    courseRepository.deleteById(id);
+                    return ResponseEntity.noContent().<Void>build();
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
