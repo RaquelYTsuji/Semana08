cb855174f0e9f5f0dde08c353e59e5ea7eedc99a
Raquel Tsuji <raqueltsuji@gmail.com>
2023-06-05 22:07:00 -0300

    Curso-Aulas: Listando Aulas

--- src/main/java/com/raquel/Semana08/model/Lesson.java
+++ src/main/java/com/raquel/Semana08/model/Lesson.java
@@ -1,5 +1,6 @@
 package com.raquel.Semana08.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -19,5 +20,6 @@ public class Lesson {
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "course_id", nullable = false)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private Course course;
 }
